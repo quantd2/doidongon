9586703f72809deccd1ec9d02c0b63be323d5a7a
quantd2 <dinhquan.tran@gmail.com>
2017-02-02 20:33:53 +0700

    add test to User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+describe User do
+  before do
+    @user = User.new(email: "user@example.com", password: "stinkydog",
+      password_confirmation: "stinkydog")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:encrypted_password) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should be_valid }
+
+  describe "when email is not present" do
+    before { @user.email = "" }
+    it { should_not be_valid }
+  end
+
+  describe "when password not present" do
+    before { @user.password = "" }
+    it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user at foo.org example.user@foo.
+        foo@bar baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).to be_invalid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+    it "should be valid" do
+      expect(@user).to be_invalid
+    end
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+end
