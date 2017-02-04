4c5b1a4010be5c03f1ad7fd4a52650ffc2921724
quantd2 <dinhquan.tran@gmail.com>
2017-02-04 11:03:15 +0700

    add test model user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,6 +64,15 @@ describe User do
 
   describe "when password confirmation is nil" do
     before { @user.password_confirmation = nil }
-    it { should_not be_valid }
+    it { should be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 end
