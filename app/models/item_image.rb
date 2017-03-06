b049942ad77202e2da6ea611b8189c3afcc3c78b
quantd2 <dinhquan.tran@gmail.com>
2017-03-06 23:31:32 +0700

    modify my item view

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -16,7 +16,9 @@
 class ItemImage < ApplicationRecord
   belongs_to :item
   has_attached_file :image,
-                    :styles => { :medium => "300x300>", :thumb => "100x100#" }
+                    :styles => { :medium => "400x400>",
+                                 :small  => "220x220#",
+                                 :thumb  => "100x100#" }
   validates_attachment_content_type :image,
       :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/,
       :message => 'file type is not allowed (only jpeg/png/gif images)'
