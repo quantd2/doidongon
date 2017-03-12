e7525c4370828114d16dc334daa088aa7bb3f243
quantd2 <dinhquan.tran@gmail.com>
2017-03-13 00:41:24 +0700

    done my-item

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -17,8 +17,8 @@ class ItemImage < ApplicationRecord
   belongs_to :item
   has_attached_file :image,
                     :styles => { :medium => "400x400>",
-                                 :small  => "220x220#",
-                                 :thumb  => "100x100#" }
+                                 :small  => "320x220#",
+                                 :thumb  => "150x100#" }
   validates_attachment_content_type :image,
       :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/,
       :message => 'file type is not allowed (only jpeg/png/gif images)'
